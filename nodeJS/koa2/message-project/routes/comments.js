7c07fcd1f632f867c5af02ae899d881f1854647b
Zhang JiaSai <1556009335@qq.com>
2021-07-06 18:21:18 +0800

    message项目前端连接mongodb添加数据获取数据

---
+++ nodeJS/koa2/message-project/routes/comments.js
@@ -0,0 +1,52 @@
+const router = require('koa-router')();
+// 引入 Comment的 model
+const {Comment} = require("../db/model");
+
+
+//path路径前缀
+router.prefix('/api')
+
+//定义list路由，获取留言板
+router.get('/list', async (ctx) => {
+    //获取querystring: ctx.query
+    const query = ctx.query;
+    let result = {};
+    if(query?.username){
+        const {username} = query;
+        //筛选查询数据库
+        result = await Comment.findOne({username});       
+    }else{
+        result = await Comment.find().sort({_id:-1});
+    }
+    //返回数据
+    ctx.body = {
+        errno:0,
+        data:result
+    };
+})
+
+//定义create路由，创建留言
+router.post('/create', async (ctx) => {
+    const body = await ctx.request.body //获取请求的body数据
+    console.log('发送来的body是：', body);
+    if(body?.content && body?.username){
+        //获取body数据
+        const {content , username } = body;
+        //插入到数据库
+       const newMessage = await Comment.create({
+            content,
+            username
+        })
+
+    }   
+    //设置返回的body,把数据带上
+    ctx.body = {
+        errno: 0,
+        message: '留言创建成功！',
+        data:newMessage
+    }
+
+})
+
+//输出
+module.exports = router;
