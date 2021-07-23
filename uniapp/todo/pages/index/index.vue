e91256763c973818ffa0959e6492b9020589bbae
Zhang JiaSai <1556009335@qq.com>
2021-07-23 18:27:38 +0800

    面试课及uniapp

---
+++ uniapp/todo/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<view class="box" hover-class="hoverClass">
+			{{title}}
+		</view>
+		<image src="../../static/logo.png" mode="aspectFit"></image>
+	<button :title="title" @click="handleClick" v-if="show">
+		{{btnText}}
+	</button>
+  </view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				btnText: '点我看看',
+				show:true
+			}
+		},
+		onLoad() {
+			this.sayHi("Hello, uni-app")
+		},
+		methods: {
+			handleClick(e) {
+				this.btnText = "你真好看"
+			},
+			sayHi(msg) {
+				alert(msg)
+			}
+		}
+	}
+</script>
+
+<style>
+	.box {
+		color: red;
+	}
+
+	.hoverClass {
+		color: #007AFF;
+	}
+</style>
---
+++ uniapp/todo/pages/test-page/test-page.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="box">
+		test-page
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
